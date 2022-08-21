c89d44d4e405a156e0959a6e4dfe0e659d6da9fb
Angel Valladares <arivalladares2.0@gmail.com>
2022-08-21 06:13:31 -0600

    union backend y frontend

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template, request, jsonify
+from chat import get_response
+import os
+
+IMG_FOLDER = os.path.join('static', 'images')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+
+@app.get("/")
+def index_get():
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'banhn.png')
+    full_filename2 = os.path.join(app.config['UPLOAD_FOLDER'],  'robot.jpg')
+    return render_template("index.html", user_image=full_filename, user_image2=full_filename2)
+
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
